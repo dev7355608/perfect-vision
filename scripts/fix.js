001331bed715a29bef6fd50c943d9654d7301031
dev7355608 <dev7355608@gmail.com>
2021-04-11 17:05:44 +0200

    Add hack to fix render-to-texture bug in Firefox (Linux)

--- scripts/fix.js
+++ scripts/fix.js
@@ -140,3 +140,46 @@ Hooks.once("init", () => {
         return res;
     });
 });
+
+// https://gitlab.com/foundrynet/foundryvtt/-/issues/4850
+Hooks.once("canvasInit", () => {
+    const renderer = canvas.app.renderer;
+    const gl = renderer.gl;
+    const pixel = new Uint8Array(4);
+    const texture = PIXI.RenderTexture.create({ width: 1, height: 1 });
+    const container = new PIXI.Container();
+
+    container.addChild(new PIXI.Graphics()).beginFill(0xffffff).drawRect(0, 0, 1, 1).endFill();
+    container.mask = container.addChild(new PIXI.Graphics());
+
+    renderer.render(container, texture, true);
+    renderer.renderTexture.bind(texture);
+
+    gl.readPixels(0, 0, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, pixel);
+
+    if (pixel[0] !== 0) {
+        renderer.render(container, texture, true);
+        renderer.renderTexture.bind(texture);
+
+        gl.readPixels(0, 0, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, pixel);
+
+        if (pixel[0] !== 0) {
+            ui.notifications.error("Rendering bug detected. Workaround is NOT working! Please switch to a Chromium-based browser!");
+        } else {
+            ui.notifications.warn("Rendering bug detected. Workaround enabled. Consider switching to a Chromium-based browser!");
+
+            const render = PIXI.Renderer.prototype.render;
+            PIXI.Renderer.prototype.render = function () {
+                render.apply(this, arguments);
+
+                if (!this.renderingToScreen) {
+                    render.apply(this, arguments);
+                }
+            }
+        }
+    }
+
+    renderer.renderTexture.bind(null);
+
+    texture.destroy(true);
+});
