0ecb6e98b67fad1421734e90a2100e5e602b57ad
dev7355608 <dev7355608@gmail.com>
2021-08-03 22:31:42 +0200

    Update patch function to respect wrapper priority

--- scripts/utils/patch.js
+++ scripts/utils/patch.js
@@ -5,77 +5,62 @@ const wrappers = {};
 export function patch(target, type, func) {
     Logger.debug("Patching %s (%s)", target, type);
 
-    let wrapper = wrappers[target];
+    let wrapper;
 
-    if (!wrapper) {
-        if (type === "PRE") {
-            wrapper = function (wrapped, ...args) {
-                return wrapped(...func.apply(this, args));
-            };
-            wrapper.type = "WRAPPER";
-        } else if (type === "POST") {
-            wrapper = function (wrapped, ...args) {
-                return func.call(this, wrapped(...args), ...args);
-            };
-            wrapper.type = "WRAPPER";
-        } else if (type === "WRAPPER" || type === "MIXED") {
-            wrapper = function (wrapped, ...args) {
-                return func.call(this, wrapped, ...args);
-            };
-            wrapper.type = type;
-        } else if (type === "OVERRIDE") {
-            wrapper = func;
-            wrapper.type = "OVERRIDE";
-        }
+    if (type === "PRE") {
+        wrapper = function (wrapped, ...args) {
+            return wrapped(...func.apply(this, args));
+        };
+        wrapper.type = "WRAPPER";
+    } else if (type === "POST") {
+        wrapper = function (wrapped, ...args) {
+            return func.call(this, wrapped(...args), ...args);
+        };
+        wrapper.type = "WRAPPER";
     } else {
-        libWrapper.unregister("perfect-vision", target);
-
-        const wrap = wrapper;
-
-        if (wrapper.type !== "OVERRIDE") {
-            if (type === "PRE") {
-                wrapper = function (wrapped, ...args) {
-                    return wrap.call(this, wrapped, ...func.apply(this, args));
-                };
-                wrapper.type = "WRAPPER";
-            } else if (type === "POST") {
-                wrapper = function (wrapped, ...args) {
-                    return func.call(this, wrap.call(this, wrapped, ...args), ...args);
-                };
-                wrapper.type = "WRAPPER";
-            } else if (type === "WRAPPER" || type === "MIXED") {
-                wrapper = function (wrapped, ...args) {
-                    return func.call(this, (...args) => wrap.call(this, wrapped, ...args), ...args);
-                };
-                wrapper.type = type;
-            } else if (type === "OVERRIDE") {
-                wrapper = function (...args) {
-                    return wrap.call(this, func.bind(this), ...args);
-                };
-                wrapper.type = "OVERRIDE";
-            }
-        } else {
-            console.assert(type !== "OVERRIDE", "OVERRIDE cannot be registered more than once!");
-
-            if (type === "PRE") {
-                wrapper = function (...args) {
-                    return wrap.apply(this, func.apply(this, args));
-                };
-            } else if (type === "POST") {
-                wrapper = function (...args) {
-                    return func.call(this, wrap.apply(this, args), ...args);
-                };
-            } else if (type === "WRAPPER" || type === "MIXED") {
-                wrapper = function (...args) {
-                    return func.call(this, wrap.bind(this), ...args);
-                };
-            }
-
-            wrapper.type = "OVERRIDE";
-        }
+        wrapper = func;
+        wrapper.type = type;
     }
 
-    wrappers[target] = wrapper;
+    if (wrapper.type === "OVERRIDE") {
+        wrapper.priority = 0;
+    } else if (wrapper.type === "MIXED") {
+        wrapper.priority = 1;
+    } else if (wrapper.type === "WRAPPER") {
+        wrapper.priority = 2;
+    }
+
+    let list = wrappers[target];
+
+    if (!list) {
+        wrappers[target] = list = [];
+    } else {
+        libWrapper.unregister("perfect-vision", target);
+    }
+
+    list.push(wrapper);
+    list.sort((a, b) => a.priority - b.priority);
+
+    wrapper = list[0];
+
+    for (let i = 1; i < list.length; i++) {
+        const last = wrapper;
+        const next = list[i];
+
+        if (last.type !== "OVERRIDE") {
+            wrapper = function (wrapped, ...args) {
+                return next.call(this, (...args) => last.call(this, wrapped, ...args), ...args);
+            };
+        } else {
+            console.assert(next.type !== "OVERRIDE", "OVERRIDE cannot be registered more than once!");
+
+            wrapper = function (...args) {
+                return next.call(this, last.bind(this), ...args);
+            };
+        }
+
+        wrapper.type = last.type;
+    }
 
     libWrapper.register("perfect-vision", target, wrapper, wrapper.type);
 }
