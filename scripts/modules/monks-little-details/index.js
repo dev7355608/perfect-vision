45c4a3f6ac69720d37ebd434af153a13616f35bb
dev7355608 <dev7355608@gmail.com>
2021-08-13 03:01:12 +0200

    Fix Monk's Little Details blood splat

--- scripts/modules/monks-little-details/index.js
+++ scripts/modules/monks-little-details/index.js
@@ -1,19 +1,37 @@
 import { Board } from "../../core/board.js";
+import { patch } from "../../utils/patch.js";
 
 Hooks.once("init", () => {
     if (!game.modules.get("monks-little-details")?.active) {
         return;
     }
 
-    Object.defineProperty(Token.prototype, "turnmarker", {
-        configurable: true,
-        get() {
-            return this._monks_little_details_turnmarker;
-        },
-        set(value) {
-            this._monks_little_details_turnmarker = value;
-
-            Board.place(`Token#${this.id}.monks-little-details.turnmarker`, value, Board.LAYERS.TOKEN_MARKERS, 2);
+    patch("Token.prototype.addChild", "POST", function (result, ...objects) {
+        for (const object of objects) {
+            if (object === this.turnmarker) {
+                Board.place(`Token#${this.id}.monks-little-details.turnmarker`, object, Board.LAYERS.TOKEN_MARKERS, 2);
+            } else if (object === this.bloodsplat) {
+                Board.place(`Token#${this.id}.monks-little-details.bloodsplat`, object, Board.LAYERS.OVERHEAD_EFFECTS, 0);
+            }
         }
-    })
+
+        return result;
+    });
+
+    patch("Token.prototype.addChildAt", "POST", function (result, object, index) {
+        if (object === this.turnmarker) {
+            Board.place(`Token#${this.id}.monks-little-details.turnmarker`, object, Board.LAYERS.TOKEN_MARKERS, 2);
+        } else if (object === this.bloodsplat) {
+            Board.place(`Token#${this.id}.monks-little-details.bloodsplat`, object, Board.LAYERS.OVERHEAD_EFFECTS, 0);
+        }
+
+        return result;
+    });
+
+    patch("Token.prototype.destroy", "PRE", function () {
+        Board.unplace(`Token#${this.id}.monks-little-details.turnmarker`);
+        Board.unplace(`Token#${this.id}.monks-little-details.bloodsplat`);
+
+        return arguments;
+    });
 });
