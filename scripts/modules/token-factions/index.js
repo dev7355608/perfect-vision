e0a40bc128365b69d092306b915282ceb90a4680
dev7355608 <dev7355608@gmail.com>
2021-09-22 02:20:10 +0200

    Fix Token Factions compatibility

--- scripts/modules/token-factions/index.js
+++ scripts/modules/token-factions/index.js
@@ -11,78 +11,97 @@ Hooks.once("init", () => {
         return false;
     };
 
-    let bevelTexture;
+    if (isNewerVersion(game.modules.get("token-factions").data.version, "0.2.1")) {
+        Object.defineProperty(Token.prototype, "factions", {
+            get() {
+                return this._pv_factions;
+            },
+            set(value) {
+                Board.place(`Token#${this.id}.factions`, value, Board.LAYERS.TOKEN_BASES, Board.Z_INDICES.PARENT);
 
-    async function loadBevelTexture() {
-        bevelTexture = await loadTexture("modules/token-factions/assets/bevel-texture.png");
-    }
+                this._pv_factions = value;
+            }
+        });
 
-    patch("TokenFactions.updateTokens", "MIXED", async function (wrapped, tokenData) {
-        if (tokenData instanceof Token && (!tokenData.id || tokenData._original)) {
-            return;
+        patch("Token.prototype.destroy", "PRE", function () {
+            Board.unplace(`Token#${this.id}.factions`);
+
+            return arguments;
+        });
+    } else {
+        let bevelTexture;
+
+        async function loadBevelTexture() {
+            bevelTexture = await loadTexture("modules/token-factions/assets/bevel-texture.png");
         }
 
-        if (!bevelTexture || !bevelTexture.baseTexture) {
-            loadBevelTexture();
-        }
-
-        return await wrapped(tokenData);
-    });
-
-    patch("TokenFactions.updateTokenBase", "OVERRIDE", function (token) {
-        if (!token.id || token._original) {
-            return;
-        }
-
-        if (token instanceof Token && token.icon && bevelTexture && bevelTexture.baseTexture) {
-            const flags = token.data.flags["token-factions"];
-            const drawFramesByDefault = game.settings.get("token-factions", "draw-frames-by-default");
-            const drawFrameOverride = flags ? flags["draw-frame"] : undefined;
-            const drawFrame = drawFrameOverride === undefined ? drawFramesByDefault : drawFrameOverride;
-            const colorFrom = game.settings.get("token-factions", "color-from");
-            let color;
-
-            if (!token.factionBase) {
-                token.factionBase = token.addChildAt(
-                    new PIXI.Container(), token.getChildIndex(token.icon) - 1,
-                );
-
-                Board.place(`Token#${token.id}.factionBase`, token.factionBase, Board.LAYERS.TOKEN_BASES, Board.Z_INDICES.PARENT);
-            } else {
-                token.factionBase.removeChildren().forEach(c => c.destroy());
+        patch("TokenFactions.updateTokens", "MIXED", async function (wrapped, tokenData) {
+            if (tokenData instanceof Token && (!tokenData.id || tokenData._original)) {
+                return;
             }
 
-            if (!token.factionFrame) {
-                token.factionFrame = token.addChildAt(
-                    new PIXI.Container(), token.getChildIndex(token.icon) + 1,
-                );
-            } else {
-                token.factionFrame.removeChildren().forEach(c => c.destroy());
+            if (!bevelTexture || !bevelTexture.baseTexture) {
+                loadBevelTexture();
             }
 
-            if (colorFrom === "token-disposition") {
-                color = TokenFactions.getDispositionColor(token);
-            } else if (colorFrom === "actor-folder-color") {
-                color = TokenFactions.getFolderColor(token);
-            } else { // colorFrom === "custom-disposition"
-                color = TokenFactions.getCustomDispositionColor(token);
+            return await wrapped(tokenData);
+        });
+
+        patch("TokenFactions.updateTokenBase", "OVERRIDE", function (token) {
+            if (!token.id || token._original) {
+                return;
             }
 
-            if (color) {
-                TokenFactions.drawBase({ color, container: token.factionBase, token });
+            if (token instanceof Token && token.icon && bevelTexture && bevelTexture.baseTexture) {
+                const flags = token.data.flags["token-factions"];
+                const drawFramesByDefault = game.settings.get("token-factions", "draw-frames-by-default");
+                const drawFrameOverride = flags ? flags["draw-frame"] : undefined;
+                const drawFrame = drawFrameOverride === undefined ? drawFramesByDefault : drawFrameOverride;
+                const colorFrom = game.settings.get("token-factions", "color-from");
+                let color;
 
-                if (drawFrame) {
-                    TokenFactions.drawFrame({ color, container: token.factionFrame, token });
+                if (!token.factionBase) {
+                    token.factionBase = token.addChildAt(
+                        new PIXI.Container(), token.getChildIndex(token.icon) - 1,
+                    );
+
+                    Board.place(`Token#${token.id}.factionBase`, token.factionBase, Board.LAYERS.TOKEN_BASES, Board.Z_INDICES.PARENT);
                 } else {
-                    TokenFactions.drawFrame({ color, container: token.factionBase, token });
+                    token.factionBase.removeChildren().forEach(c => c.destroy());
+                }
+
+                if (!token.factionFrame) {
+                    token.factionFrame = token.addChildAt(
+                        new PIXI.Container(), token.getChildIndex(token.icon) + 1,
+                    );
+                } else {
+                    token.factionFrame.removeChildren().forEach(c => c.destroy());
+                }
+
+                if (colorFrom === "token-disposition") {
+                    color = TokenFactions.getDispositionColor(token);
+                } else if (colorFrom === "actor-folder-color") {
+                    color = TokenFactions.getFolderColor(token);
+                } else { // colorFrom === "custom-disposition"
+                    color = TokenFactions.getCustomDispositionColor(token);
+                }
+
+                if (color) {
+                    TokenFactions.drawBase({ color, container: token.factionBase, token });
+
+                    if (drawFrame) {
+                        TokenFactions.drawFrame({ color, container: token.factionFrame, token });
+                    } else {
+                        TokenFactions.drawFrame({ color, container: token.factionBase, token });
+                    }
                 }
             }
-        }
-    });
+        });
 
-    patch("Token.prototype.destroy", "PRE", function () {
-        Board.unplace(`Token#${this.id}.factionBase`);
+        patch("Token.prototype.destroy", "PRE", function () {
+            Board.unplace(`Token#${this.id}.factionBase`);
 
-        return arguments;
-    });
+            return arguments;
+        });
+    }
 });
