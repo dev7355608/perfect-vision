16e8b997ccc4e8eff0201e3daaf1ef3365480baf
dev7355608 <dev7355608@gmail.com>
2021-12-28 00:38:44 +0100

    Move walls layer to the interface group

--- scripts/core/walls.js
+++ scripts/core/walls.js
@@ -2,6 +2,12 @@ import { patch } from "../utils/patch.js";
 import { TransformedShape } from "../utils/transformed-shape.js";
 
 Hooks.once("init", () => {
+    patch("WallsLayer.prototype.draw", "WRAPPER", async function (wrapped, ...args) {
+        canvas.interface.addChild(this);
+
+        return await wrapped(...args);
+    });
+
     patch("WallsLayer.prototype._createBoundaries", "OVERRIDE", function () {
         // Boundaries are padded outwards by the grid size to allow lighting effects to be cleanly masked at the edges
         let { width, height, size } = canvas.dimensions;
