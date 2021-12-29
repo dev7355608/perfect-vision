a552a31e21d58552e4f74bfb25c0b4700dcecd7d
dev7355608 <dev7355608@gmail.com>
2021-12-29 16:30:04 +0100

    Fix weather mask not being disposed

--- scripts/core/weather.js
+++ scripts/core/weather.js
@@ -90,23 +90,23 @@ WeatherLayer.prototype._pv_updateMask = function (visible) {
             this.weather.mask = null;
         }
 
-        if (this.mask) {
-            this.mask.destroy(true);
-        }
-
-        if (visible && canvas.scene.img) {
-            this.mask = this.addChild(new PIXI.LegacyGraphics().beginFill().drawShape(canvas.dimensions.sceneRect).endFill());
-        } else {
-            this.mask = null;
-        }
-
         this.weather.visible = visible;
+    }
 
-        if (this._pv_visible !== visible) {
-            this._pv_visible = visible;
+    if (this.mask) {
+        this.mask.destroy(true);
+    }
 
-            canvas.weather._pv_refreshBuffer();
-        }
+    if (visible && canvas.scene.img) {
+        this.mask = this.addChild(new PIXI.LegacyGraphics().beginFill().drawShape(canvas.dimensions.sceneRect).endFill());
+    } else {
+        this.mask = null;
+    }
+
+    if (this._pv_visible !== visible) {
+        this._pv_visible = visible;
+
+        canvas.weather._pv_refreshBuffer();
     }
 };
 
