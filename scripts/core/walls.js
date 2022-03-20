2e7acb14ff3d1c963b2838ba54e248cd5983a2d3
dev7355608 <dev7355608@gmail.com>
2022-03-20 02:00:51 +0100

    Filter cutout tiles in identifyInteriorState

--- scripts/core/walls.js
+++ scripts/core/walls.js
@@ -6,4 +6,20 @@ Hooks.once("init", () => {
 
         return await wrapped(...args);
     });
+
+    patch("Wall.prototype.identifyInteriorState", "OVERRIDE", function () {
+        this.roof = null;
+
+        for (const roof of canvas.foreground.roofs) {
+            if (roof.document.getFlag("betterroofs", "brMode") === 3) {
+                continue;
+            }
+
+            const isInterior = roof.containsPixel(this.data.c[0], this.data.c[1]) && roof.containsPixel(this.data.c[2], this.data.c[3]);
+
+            if (isInterior) {
+                this.roof = roof;
+            }
+        }
+    });
 });
