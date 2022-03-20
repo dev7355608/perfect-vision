fa3a32256aab9750546227ae7bfbf182ae9d1339
dev7355608 <dev7355608@gmail.com>
2022-03-20 02:52:18 +0100

    Don't patch identifyInteriorState if Better Roofs is inactive

--- scripts/compatibility/betterroofs.js
+++ scripts/compatibility/betterroofs.js
@@ -5,6 +5,22 @@ Hooks.once("init", () => {
         return;
     }
 
+    patch("Wall.prototype.identifyInteriorState", "OVERRIDE", function () {
+        this.roof = null;
+
+        for (const roof of canvas.foreground.roofs) {
+            if (roof.document.getFlag("betterroofs", "brMode") === 3) {
+                continue;
+            }
+
+            const isInterior = roof.containsPixel(this.data.c[0], this.data.c[1]) && roof.containsPixel(this.data.c[2], this.data.c[3]);
+
+            if (isInterior) {
+                this.roof = roof;
+            }
+        }
+    });
+
     patch("betterRoofsHelpers.prototype.computeMask", "OVERRIDE", function (tile, controlledToken) { });
 
     Hooks.on("updateTile", (document, change, options, userId, arg) => {
