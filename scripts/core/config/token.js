f9277d258a944cc324d6f724f8fd0b478b557bf2
dev7355608 <dev7355608@gmail.com>
2022-04-15 20:17:22 +0200

    Refactor token config

--- scripts/core/config/token.js
+++ scripts/core/config/token.js
@@ -1,52 +1,23 @@
-import { patch } from "../../utils/patch.js";
-
-Hooks.once("init", () => {
-    patch("TokenConfig.prototype._onChangeInput", "WRAPPER", async function (wrapped, event, ...args) {
-        const target = event.target;
-        const name = target.name || target.id;
-
-        if (name === "light.luminosity") {
-            target.form.elements["flags.core.priority"].placeholder = target.value >= 0 ? 0 : 10;
-        }
-
-        return wrapped(event, ...args);
-    });
-
-    patch("TokenConfig.prototype._getSubmitData", "POST", function (data) {
-        if (!Number.isFinite(data["flags.core.priority"])) {
-            delete data["flags.core.priority"];
-
-            data["flags.core.-=priority"] = null;
-        }
-
-        if (!this.form.elements["perfect-vision.light.sightLimit.enable"].checked) {
-            delete data["flags.perfect-vision.light.sightLimit"];
-
-            data["flags.perfect-vision.light.-=sightLimit"] = null;
-        }
-
-        return data;
-    });
-});
-
-Hooks.on("renderTokenConfig", (sheet, html, data) => {
+Hooks.on("renderTokenConfig", (sheet, html) => {
+    const form = sheet.form;
     const document = sheet.token;
+    const gridUnits = (document.parent?.data.gridUnits ?? game.system.data.gridUnits) || "Grid Units";
 
-    html.find(`div[data-tab="vision"]`).append(`\
+    html.find(`.tab[data-tab="vision"]`).append(`\
         <div class="form-group">
-            <label>Sight Limit <span class="units">(${(document.parent?.data.gridUnits ?? game.system.data.gridUnits) || "Grid Units"})</span></label>
+            <label>Sight Limit <span class="units">(${gridUnits})</span></label>
             <div class="form-fields">
-                <input type="number" min="0.0" step="0.1" name="flags.perfect-vision.sightLimit" placeholder="Unlimited" data-dtype="Number">
+                <input type="number" data-dtype="Number" name="flags.perfect-vision.sightLimit" min="0.0" step="0.1" placeholder="Unlimited">
             </div>
         </div>`);
     html.find(`input[name="flags.perfect-vision.sightLimit"]`)
         .attr("value", document.getFlag("perfect-vision", "sightLimit"));
 
-    html.find(`div[data-tab="light"] > div[data-tab="advanced"]`).append(`\
+    html.find(`.tab[data-tab="light"] > .tab[data-tab="advanced"]`).append(`\
         <div class="form-group">
             <label>Priority</label>
             <div class="form-fields">
-                <input type="number" name="flags.core.priority" placeholder="0" data-dtype="Number">
+                <input type="number" data-dtype="Number" name="flags.core.priority" step="1" placeholder="0">
             </div>
             <p class="hint">
                 Higher priority light sources are rendered above lower priority light sources.
@@ -54,10 +25,11 @@ Hooks.on("renderTokenConfig", (sheet, html, data) => {
             </p>
         </div>
         <div class="form-group">
-            <label>Sight Limit <span class="units">(${(document.parent?.data.gridUnits ?? game.system.data.gridUnits) || "Grid Units"})</span></label>
+            <label>Sight Limit <span class="units">(${gridUnits})</span></label>
             <div class="form-fields">
-                <label class="checkbox">Enable <input type="checkbox" id="perfect-vision.light.sightLimit.enable"></label>
-                <input type="number" min="0.0" step="0.1" name="flags.perfect-vision.light.sightLimit" placeholder="Infinity" data-dtype="Number">
+                <label class="checkbox">Enable <input type="checkbox" id="perfect-vision.light.sightLimit:enable"></label>
+                <input type="number" data-dtype="Number" name="flags.perfect-vision.light.sightLimit" min="0.0" step="0.1" disabled>
+                <input type="hidden" data-dtype="Number" name="flags.perfect-vision.light.-=sightLimit">
             </div>
             <p class="hint">
                 If enabled, in the area of the light source tokens can see at least as far as the limit if the luminosity is greater or equal to zero,
@@ -66,12 +38,36 @@ Hooks.on("renderTokenConfig", (sheet, html, data) => {
         </div>`);
 
     html.find(`input[name="flags.core.priority"]`)
-        .attr("value", document.getFlag("core", "priority") ?? null)
+        .attr("value", document.getFlag("core", "priority"))
         .attr("placeholder", document.data.light.luminosity >= 0 ? 0 : 10);
-    html.find(`input[id="perfect-vision.light.sightLimit.enable"]`)
+    html.find(`input[id="perfect-vision.light.sightLimit:enable"]`)
         .attr("checked", document.getFlag("perfect-vision", "light.sightLimit") !== undefined);
     html.find(`input[name="flags.perfect-vision.light.sightLimit"]`)
         .attr("value", document.getFlag("perfect-vision", "light.sightLimit"));
+
+    const updateSightLimit = () => {
+        const enabled = form.elements["perfect-vision.light.sightLimit:enable"].checked;
+
+        form.elements["flags.perfect-vision.light.sightLimit"].disabled = !enabled;
+        form.elements["flags.perfect-vision.light.-=sightLimit"].disabled = enabled;
+
+        if (form.elements["flags.perfect-vision.light.sightLimit"].disabled) {
+            form.elements["flags.perfect-vision.light.sightLimit"].value = null;
+            $(form.elements["flags.perfect-vision.light.sightLimit"]).attr("placeholder", "");
+        } else {
+            $(form.elements["flags.perfect-vision.light.sightLimit"]).attr("placeholder", "Infinity");
+        }
+    };
+
+    updateSightLimit();
+
+    html.find(`input[id="perfect-vision.light.sightLimit:enable"],input[name="flags.perfect-vision.light.sightLimit"]`)
+        .each(function () {
+            this.addEventListener("change", updateSightLimit, { capture: true });
+        });
+
+    html.find(`input[name="light.luminosity"]`)
+        .on("change", event => html.find(`input[name="flags.core.priority"]`).attr("placeholder", event.target.value >= 0 ? 0 : 10));
 
     sheet.options.height = "auto";
     sheet.position.height = "auto";
