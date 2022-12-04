c3b9ea14fbcfda84ed401b272a1431c9f8016d49
dev7355608 <dev7355608@gmail.com>
2022-12-04 22:57:14 +0100

    Fix regression

--- patches/index.js
+++ patches/index.js
@@ -1,32 +1,34 @@
 import "./fog.js";
 import "./mask.js";
 
-if (!isNewerVersion(game.version, "10.290")) {
-    InverseOcclusionSamplerShader.fragmentShader = `
-    precision ${PIXI.settings.PRECISION_FRAGMENT} float;
-    varying vec2 vUvs;
-    varying vec2 vUvsMask;
-    uniform vec4 tintAlpha;
-    uniform sampler2D sampler;
-    uniform sampler2D maskSampler;
-    uniform float alphaOcclusion;
-    uniform float alpha;
-    uniform float depthElevation;
-    uniform bool roof;
-    uniform bool vision;
-    void main() {
-      vec4 otex = texture2D(maskSampler, vUvsMask);
-      float occlusionElevation = roof ? otex.a : (vision ? otex.b : otex.g);
-      float tex = 1.0 - step(depthElevation, occlusionElevation);
-      float mask = 1.0 - tex + (alphaOcclusion * tex);
-      float calpha = tex + alpha * (1.0 - tex);
-      gl_FragColor = texture2D(sampler, vUvs) * mask * calpha * tintAlpha;
-    }
-    `;
+Hooks.once("init", () => {
+    if (!isNewerVersion(game.version, "10.290")) {
+        InverseOcclusionSamplerShader.fragmentShader = `
+        precision ${PIXI.settings.PRECISION_FRAGMENT} float;
+        varying vec2 vUvs;
+        varying vec2 vUvsMask;
+        uniform vec4 tintAlpha;
+        uniform sampler2D sampler;
+        uniform sampler2D maskSampler;
+        uniform float alphaOcclusion;
+        uniform float alpha;
+        uniform float depthElevation;
+        uniform bool roof;
+        uniform bool vision;
+        void main() {
+        vec4 otex = texture2D(maskSampler, vUvsMask);
+        float occlusionElevation = roof ? otex.a : (vision ? otex.b : otex.g);
+        float tex = 1.0 - step(depthElevation, occlusionElevation);
+        float mask = 1.0 - tex + (alphaOcclusion * tex);
+        float calpha = tex + alpha * (1.0 - tex);
+        gl_FragColor = texture2D(sampler, vUvs) * mask * calpha * tintAlpha;
+        }
+        `;
 
-    OcclusionSamplerShader.batchFragmentShader = OcclusionSamplerShader.batchFragmentShader.replace(
-        /\bvDepthElevation \* rAlpha \* vColor\.a\b/, "rAlpha * vColor.a");
-}
+        OcclusionSamplerShader.batchFragmentShader = OcclusionSamplerShader.batchFragmentShader.replace(
+            /\bvDepthElevation \* rAlpha \* vColor\.a\b/, "rAlpha * vColor.a");
+    }
+});
 
 Drawing.prototype._onUpdate = function (changed, options, userId) {
     // Fully re-draw when some drawing elements have changed
